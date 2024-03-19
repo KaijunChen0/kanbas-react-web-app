204f23c38f68eee6a81859145fdfb22bdb10198a
KaijunChen0 <113864900+KaijunChen0@users.noreply.github.com>
2024-03-18 19:44:46 -0700

    a5 3.2

--- src/Labs/a5/WorkingWithObjects.tsx
+++ src/Labs/a5/WorkingWithObjects.tsx
@@ -8,6 +8,14 @@ function WorkingWithObjects() {
       });
     const ASSIGNMENT_URL = "http://localhost:4000/a5/assignment";
 
+    const [module, setModule] = useState({
+        id: 12345,
+        name: "NodeJS",
+        description: "NodeJS is a JavaScript runtime built on Chrome's V8 JavaScript engine",
+        course: "CS5610",
+    });
+    const MODULE_URL = "http://localhost:4000/a5/module";
+
   return (
     <div>
         <h3>Working With Objects</h3>
@@ -35,6 +43,64 @@ function WorkingWithObjects() {
             className="btn btn-primary">
             Update Title
         </a> 
+
+        <h3>Exercise 1: Create a Module Object</h3>
+        <h4>Retrieving Objects</h4>
+        <a href="http://localhost:4000/a5/module"
+            role="button"
+            className="btn btn-success">
+            Get Module
+        </a>
+        <h4>Retrieving Name of Module</h4>
+        <a href="http://localhost:4000/a5/module/name"
+            role="button"
+            className="btn btn-success">
+            Get Name of Module
+        </a>
+        <h4>Modifying Name of Module</h4>
+        <input type="text" 
+            onChange={(e) => setModule({ ...module,
+                name: e.target.value })} 
+            value={module.name}/> &nbsp;
+        <a href={`${MODULE_URL}/name/${module.name}`}
+            role="button"
+            className="btn btn-success">
+            Update Name of Module
+        </a>
+
+        <h3>Exercise 2: Modify Score, Completed and Description of Assignment</h3>
+        <h4>Modifying Score</h4>
+        <input type="number" 
+            onChange={(e) => setAssignment({ ...assignment,
+                score: Number(e.target.value) })}
+            value={assignment.score}/> &nbsp;
+        <a href={`${ASSIGNMENT_URL}/score/${assignment.score}`}
+            role="button"
+            className="btn btn-warning">
+            Update Score of Assignment
+        </a> 
+
+        <h4>Modifying Completed</h4>
+        <input type="checkbox" 
+            onChange={(e) => setAssignment({ ...assignment,
+                completed: e.target.checked })}
+            checked={assignment.completed}/> &nbsp;
+        <a href={`${ASSIGNMENT_URL}/completed/${assignment.completed}`}
+            role="button"
+            className="btn btn-warning">
+            Update Completed of Assignment
+        </a>
+
+        <h4>Modifying Description</h4>
+        <input type="text" 
+            onChange={(e) => setAssignment({ ...assignment,
+                description: e.target.value })}
+            value={assignment.description}/> &nbsp;
+        <a href={`${ASSIGNMENT_URL}/description/${assignment.description}`}
+            role="button"
+            className="btn btn-warning">
+            Update Description of Assignment
+        </a>
     </div>
   );
 }
