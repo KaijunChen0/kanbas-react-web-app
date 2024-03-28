94bd40ce729db77872977ffaac1ae329e385128b
KaijunChen0 <113864900+KaijunChen0@users.noreply.github.com>
2024-03-28 15:59:19 -0700

    add env setting-1

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 // const COURSES_API = "http://localhost:4000/api/courses";
-const COURSES_API = "https://kanbas-node-server-app-0k43.onrender.com/api/courses";//replace with render.com remote server
+// const COURSES_API = "https://kanbas-node-server-app-0k43.onrender.com/api/courses";//replace with render.com remote server
+const API_BASE = process.env.REACT_APP_API_BASE;
+const COURSES_API = `${API_BASE}/api/courses`;
 
 export const findAllCourses = async () => {
     const response = await axios.get(COURSES_API);
